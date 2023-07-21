38d159fa819750520f056b9e75451601e36d6742
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-07-21 19:43:16 -0400

    Add back

---
+++ backend/models/user.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Некорректный email',
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+
+  name: {
+    default: 'Жак-Ив Кусто',
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+
+  about: {
+    default: 'Исследователь',
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+
+  avatar: {
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
+  },
+});
+
+userSchema.methods.toJSON = function toJson() {
+  const user = this.toObject();
+  delete user.password;
+
+  return user;
+};
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
