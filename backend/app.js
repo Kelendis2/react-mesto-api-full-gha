6ce0351d28b35fb92af3c834cac19283aa4e4720
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-07-29 16:05:09 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const router = require('./routes');
 const app = express();
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://api:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
